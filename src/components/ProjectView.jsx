0002624506b01700cb7180fe69b242c60318c108
Andreea Daniela Burnaz <danielaburnaz@gmail.com>
2025-09-18 01:08:02 +0300

    no more 3d

--- src/components/ProjectView.jsx
+++ src/components/ProjectView.jsx
@@ -1,6 +1,7 @@
 import Spline from '@splinetool/react-spline';
 
 export default function ProjectView() {
+  return <img src="/img/pc.png" />;
   return (
     <Spline scene="https://prod.spline.design/Q8KBtx9GhgP5D99y/scene.splinecode" />
   );
